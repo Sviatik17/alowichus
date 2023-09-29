fed547d7ad95ab7445a9d632c9277fa112e980cf
Sviatoslav <sviatos1706@gmail.com>
2023-09-29 20:16:06 +0300

    add img and style

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path=require('path');
+
+const app=express();
+
+const PORT=3000;
+
+
+// app.use(express.static(path.join(__dirname,'public')));
+
+// app.get('/',(req,res)=>{
+//     res.sendFile(path.join(__dirname,'public','index.html'))
+// })
+// app.get('/contact',(req,res)=>{
+//     res.sendFile(path.join(__dirname,'public','contact.html'))
+// })
+
+// app.listen(PORT,()=>{
+//     console.log(`Server is runnig at http:localhost:${PORT}`)
+// })
